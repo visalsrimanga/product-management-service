1fa88bdb089bb8f0495cda5e944857f16969d0ae
Visal Srimanga <109058710+visalsrimanga@users.noreply.github.com>
2024-08-12 20:18:05 +0530

    Add files via upload

---
+++ productmanagementservice/src/main/java/com/efuture/productmanagementservice/ProductmanagementserviceApplication.java
@@ -0,0 +1,13 @@
+package com.efuture.productmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductmanagementserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductmanagementserviceApplication.class, args);
+	}
+
+}
---
+++ productmanagementservice/src/test/java/com/efuture/productmanagementservice/ProductmanagementserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efuture.productmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductmanagementserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
